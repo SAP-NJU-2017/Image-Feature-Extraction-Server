e0ed2c0411aad20ff75a16412bb66879f5fd687d
AvenChang <530685625@qq.com>
2018-06-30 14:45:43 +0800

    新增保存向量方法

---
+++ app/util/exception.py
@@ -0,0 +1,15 @@
+
+class NotImageError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+
+class PixelsTooLargeError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
