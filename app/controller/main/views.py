bdc314e8afefc2e50e330ea6352fcb488eba4f6d
151250195 <151250195@smail.nju.edu.cn>
2018-06-30 13:48:38 +0800

    修改view

--- app/controller/main/views.py
+++ app/controller/main/views.py
@@ -3,9 +3,12 @@ from . import app
 from app import db
 from app.models import User as user
 from app.util import sap_api
+from app.util import sliding_window
 from flask import request
+from PIL import Image
 import requests
 import json
+import os
 
 
 # @app.route('/', methods=['GET'])
@@ -45,7 +48,7 @@ def try_similarity_scoring_api():
     filename = 'doc/Images/'
     filename = filename + request.args.get('name')  # ?key=value
     file = open(filename, 'rb')
-    result = sap_api.image_classification(image=file)
+    result = sap_api.image_classification(image=file, options={"numSimilarVectors":1})
     return result
 
 
@@ -54,6 +57,15 @@ def try_similarity_scoring_api():
 def find_error_area():
     filepath = request.args.get('path')
     # TODO 获得框的位置和大小
+    image_model_path = "./doc/ImageModel/"
+    model_list = os.listdir(image_model_path)
+    for i in range(0, len(model_list)):
+        model_path = os.path.join(image_model_path, model_list[i])
+        model = Image.open(model_path)
+        sliding_cut_list = sliding_window.slide(filepath, model.size[0], model.size[1], model.size[0]/4, model.size[1]/4)
+        image_collection = []
+
+
     result = {'startx': 0, 'starty': 0, 'endx': 0, 'endy': 0}
 
     return json.dumps(result)
--- app/util/sliding_window.py
+++ app/util/sliding_window.py
@@ -9,19 +9,19 @@ def slide(image_path, window_width, window_height, w_offset, h_offset):
     start_x = 0
     start_y = 0
     i = 0
-    cut_result = []
+    cut_result = {}
 
     if start_y + window_height > image_height or start_x + window_width > image_width:
         cut_path = './doc/SlideWindowCuts/' + str(i) + "." + image_path.split('.')[1]
         image.save(cut_path)
-        cut_result.append(cut_path)
+        cut_result[cut_path]= {'startX': start_x, 'startY': start_y}
 
     while start_y + window_height <= image_height:
         while start_x + window_width <= image_width:
             image_cut = image.crop((start_x, start_y, start_x + window_width, start_y + window_height))
             cut_path = './doc/SlideWindowCuts/' + str(i) + "." + image_path.split('.')[1]
             image_cut.save(cut_path)
-            cut_result.append(cut_path)
+            cut_result[cut_path] = {'startX': start_x, 'startY': start_y}
             start_x += w_offset
             i += 1
         start_y += h_offset
